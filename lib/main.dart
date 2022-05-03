b3647cb782c2cff43ee0d73d8b4e9dc047a7c4f6
Rafael Alves da Costa <rafalves.costa@gmail.com>
2022-05-03 07:10:20 -0300

    step-4

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,5 @@
+import 'dart:async';
+
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:flutter_hooks/flutter_hooks.dart';
@@ -24,28 +26,37 @@ void main() {
   );
 }
 
-const url = 'https://bit.ly/3qYOtDm';
+class CountDown extends ValueNotifier<int> {
+  late StreamSubscription sub;
+  CountDown({required from}) : super(from) {
+    sub = Stream.periodic(
+      const Duration(seconds: 1),
+      (v) => from - v,
+    ).takeWhile((value) => value >= 0).listen((value) {
+      this.value = value;
+    });
+  }
+
+  @override
+  void dispose() {
+    sub.cancel();
+    super.dispose();
+  }
+}
 
 class HomePage extends HookWidget {
   const HomePage({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    final future = useMemoized(() => NetworkAssetBundle(Uri.parse(url))
-        .load(url)
-        .then((data) => data.buffer.asUint8List())
-        .then((data) => Image.memory(data)));
-
-    final snapshot = useFuture(future);
-
+    final countDown = useMemoized(() => CountDown(from: 20));
+    final notifier = useListenable(countDown);
     return Scaffold(
       appBar: AppBar(
         title: const Text('Home Page'),
       ),
-      body: Column(
-        children: [
-          snapshot.data,
-        ].compactMap().toList(),
+      body: Text(
+        notifier.value.toString(),
       ),
     );
   }
