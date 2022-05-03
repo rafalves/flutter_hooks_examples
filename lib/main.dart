9ac6ce86df0896954327fa74dfd426b3dfba0dc0
Rafael Alves da Costa <rafalves.costa@gmail.com>
2022-05-02 22:19:24 -0300

    step-3

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,15 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:flutter_hooks/flutter_hooks.dart';
+
+extension CompactMap<T> on Iterable<T?> {
+  Iterable<T> compactMap<E>([
+    E? Function(T?)? transform,
+  ]) =>
+      map(
+        transform ?? (e) => e,
+      ).where((e) => e != null).cast();
+}
 
 void main() {
   runApp(
@@ -14,33 +24,28 @@ void main() {
   );
 }
 
+const url = 'https://bit.ly/3qYOtDm';
+
 class HomePage extends HookWidget {
   const HomePage({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    final controller = useTextEditingController();
-    final text = useState('');
-    useEffect(
-      () {
-        controller.addListener(() {
-          text.value = controller.text;
-        });
-        return null;
-      },
-      [controller],
-    );
+    final future = useMemoized(() => NetworkAssetBundle(Uri.parse(url))
+        .load(url)
+        .then((data) => data.buffer.asUint8List())
+        .then((data) => Image.memory(data)));
+
+    final snapshot = useFuture(future);
+
     return Scaffold(
       appBar: AppBar(
         title: const Text('Home Page'),
       ),
       body: Column(
         children: [
-          TextField(
-            controller: controller,
-          ),
-          Text('You typed: ${text.value}'),
-        ],
+          snapshot.data,
+        ].compactMap().toList(),
       ),
     );
   }
