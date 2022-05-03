78a1e6616efa97dde9d418561d8697416e0f1218
Rafael Alves da Costa <rafalves.costa@gmail.com>
2022-05-03 13:07:44 -0300

    step-7

--- lib/main.dart
+++ lib/main.dart
@@ -1,18 +1,7 @@
-import 'dart:async';
 import 'dart:math';
 
 import 'package:flutter/material.dart';
-import 'package:flutter/services.dart';
 import 'package:flutter_hooks/flutter_hooks.dart';
-
-extension CompactMap<T> on Iterable<T?> {
-  Iterable<T> compactMap<E>([
-    E? Function(T?)? transform,
-  ]) =>
-      map(
-        transform ?? (e) => e,
-      ).where((e) => e != null).cast();
-}
 
 void main() {
   runApp(
@@ -29,40 +18,170 @@ void main() {
 
 const url = 'https://bit.ly/3x7J5Qt';
 
+enum Action {
+  rotateLeft,
+  rotateRight,
+  moreVisible,
+  lessVisible,
+}
+
+class State {
+  final double rotatingDeg;
+  final double alpha;
+
+  const State({
+    required this.rotatingDeg,
+    required this.alpha,
+  });
+
+  const State.zero()
+      : rotatingDeg = 0.0,
+        alpha = 1.0;
+
+  State rotationRight() => State(
+        alpha: alpha,
+        rotatingDeg: rotatingDeg + 10,
+      );
+  State rotationLeft() => State(
+        alpha: alpha,
+        rotatingDeg: rotatingDeg - 10,
+      );
+  State increaseAlpha() => State(
+        alpha: min(alpha + 0.1, 1.0),
+        rotatingDeg: rotatingDeg,
+      );
+  State decreaseAlpha() => State(
+        alpha: max(alpha - 0.1, 0.0),
+        rotatingDeg: rotatingDeg,
+      );
+}
+
+State reducer(State oldState, Action? action) {
+  switch (action) {
+    case Action.rotateLeft:
+      return oldState.rotationLeft();
+    case Action.rotateRight:
+      return oldState.rotationRight();
+    case Action.moreVisible:
+      return oldState.increaseAlpha();
+    case Action.lessVisible:
+      return oldState.decreaseAlpha();
+    case null:
+      return oldState;
+  }
+}
+
 class HomePage extends HookWidget {
   const HomePage({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    late final StreamController<double> controller;
-    controller = useStreamController<double>(onListen: () {
-      controller.sink.add(0.0);
-    });
+    final store = useReducer<State, Action?>(
+      reducer,
+      initialState: const State.zero(),
+      initialAction: null,
+    );
     return Scaffold(
       appBar: AppBar(
         title: const Text('Home Page'),
       ),
-      body: StreamBuilder<double>(
-        stream: controller.stream,
-        builder: (context, snapshot) {
-          if (!snapshot.hasData) {
-            return const CircularProgressIndicator();
-          } else {
-            final rotation = snapshot.data ?? 0.0;
-            return GestureDetector(
-              onTap: () {
-                controller.sink.add(rotation + 10.0);
-              },
-              child: RotationTransition(
-                turns: AlwaysStoppedAnimation(rotation / 360.0),
-                child: Center(
-                  child: Image.network(url),
-                ),
-              ),
-            );
-          }
-        },
+      body: Column(
+        children: [
+          Row(
+            mainAxisAlignment: MainAxisAlignment.spaceAround,
+            children: [
+              RotateLeftButton(store: store),
+              RotateRightButton(store: store),
+              IncreaseOpacity(store: store),
+              DecreaseOpacity(store: store),
+            ],
+          ),
+          const SizedBox(height: 100),
+          const SizedBox(),
+          Opacity(
+            opacity: store.state.alpha,
+            child: RotationTransition(
+                turns: AlwaysStoppedAnimation(store.state.rotatingDeg / 360.0),
+                child: Image.network(url)),
+          ),
+        ],
       ),
+    );
+  }
+}
+
+class DecreaseOpacity extends StatelessWidget {
+  const DecreaseOpacity({
+    Key? key,
+    required this.store,
+  }) : super(key: key);
+
+  final Store<State, Action?> store;
+
+  @override
+  Widget build(BuildContext context) {
+    return TextButton(
+      onPressed: () {
+        store.dispatch(Action.lessVisible);
+      },
+      child: const Text('- Opacity'),
+    );
+  }
+}
+
+class IncreaseOpacity extends StatelessWidget {
+  const IncreaseOpacity({
+    Key? key,
+    required this.store,
+  }) : super(key: key);
+
+  final Store<State, Action?> store;
+
+  @override
+  Widget build(BuildContext context) {
+    return TextButton(
+      onPressed: () {
+        store.dispatch(Action.moreVisible);
+      },
+      child: const Text('+ Opacity'),
+    );
+  }
+}
+
+class RotateRightButton extends StatelessWidget {
+  const RotateRightButton({
+    Key? key,
+    required this.store,
+  }) : super(key: key);
+
+  final Store<State, Action?> store;
+
+  @override
+  Widget build(BuildContext context) {
+    return TextButton(
+      onPressed: () {
+        store.dispatch(Action.rotateRight);
+      },
+      child: const Text('Rotate Right'),
+    );
+  }
+}
+
+class RotateLeftButton extends StatelessWidget {
+  const RotateLeftButton({
+    Key? key,
+    required this.store,
+  }) : super(key: key);
+
+  final Store<State, Action?> store;
+
+  @override
+  Widget build(BuildContext context) {
+    return TextButton(
+      onPressed: () {
+        store.dispatch(Action.rotateLeft);
+      },
+      child: const Text('Rotate Left'),
     );
   }
 }
